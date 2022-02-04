d3fa26e58f71086febaca4472ace3c91044eb8dd
Akash Kumar Singh <akashkumarsingh11032001@gmail.com>
2022-02-04 14:24:43 +0530

    React

--- textutils/src/App.js
+++ textutils/src/App.js
@@ -1,6 +1,7 @@
 import Navbar from './components/Navbar';
-import TextForm from './components/TextForm';
+// import TextForm from './components/TextForm';
 import './App.css';
+import About from './components/About';
 
 function App() {
   return (
@@ -9,9 +10,12 @@ function App() {
       <Navbar title="TextUtiles" aboutText="About Us" />
 
       {/* Text-area */}
-      <div className="container">
+      {/* <div className="container">
         <TextForm name = "Enter your text here"/>
-      </div>
+      </div> */}
+
+      {/* About -Dark mode */}
+      <About/>
 
     </>
   );
