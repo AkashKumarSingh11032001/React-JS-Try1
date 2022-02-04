3ce5ac83add824339181e05277f0e6390c87f8d1
Akash Kumar Singh <akashkumarsingh11032001@gmail.com>
2022-02-05 00:34:33 +0530

    React

--- textutils/src/App.js
+++ textutils/src/App.js
@@ -2,12 +2,22 @@ import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
 import './App.css';
 import About from './components/About';
+import React, { useState } from 'react'
 
 function App() {
+  const [mode,setMode] = useState("light");
+  const toggleMode = () =>{
+    if(mode === "light"){
+      setMode("dark")
+    }
+    else{
+      setMode("light")
+    }
+  }
   return (
     <>
       {/* NavBar */}
-      <Navbar title="TextUtiles" aboutText="About Us" />
+      <Navbar title="TextUtiles" aboutText="About Us" mode={mode} toggleMode={toggleMode}/>
 
       {/* Text-area */}
       <div className="container">
