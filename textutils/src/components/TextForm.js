149ac812b8f29550e45c6f350521c2b22948f914
Akash Kumar Singh <akashkumarsingh11032001@gmail.com>
2022-02-04 10:29:02 +0530

    React

--- textutils/src/components/TextForm.js
+++ textutils/src/components/TextForm.js
@@ -1,14 +1,26 @@
-import React from 'react'
+import React, {useState} from 'react'
 import PropTypes from 'prop-types'
 
 export default function TextForm(props) {
+    // button action declaration
+    const handleUpClick= () =>{
+        console.log("Upper case was clicked")
+        setText("Uppercase was clicked")
+    }
+    const handleOnChange= () =>{
+        console.log("OnChnage was clicked")
+    }
+    
+    // setting useState properties
+    const [text,setText] = useState("Enter Your Text Here!")
+    
     return (
         <div>
             <h1>{props.name}</h1>
             <div className="mb-3">
-                <textarea className="form-control" id="mybox" rows="8"></textarea>
+                <textarea className="form-control" value={text} onChange = {handleOnChange} id="mybox" rows="8"></textarea>
             </div>
-            <button className = "btn btn-primary">Convert To UpperCase</button>
+            <button className = "btn btn-primary" onClick={handleUpClick}>Convert To UpperCase</button>
         </div>
     );
 }
