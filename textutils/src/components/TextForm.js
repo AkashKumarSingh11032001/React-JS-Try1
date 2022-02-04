5d949e0a35b78484007606c5eb8d56fbc1ced92a
Akash Kumar Singh <akashkumarsingh11032001@gmail.com>
2022-02-04 10:41:48 +0530

    React

--- textutils/src/components/TextForm.js
+++ textutils/src/components/TextForm.js
@@ -8,6 +8,16 @@ export default function TextForm(props) {
         let newText = text.toUpperCase()
         setText(newText)
     }
+    const handleLoClick = () =>{
+        console.log("Lower case was clicked")
+        let newText = text.toLowerCase()
+        setText(newText)
+    }
+    const handleLenClick = () =>{
+        console.log("Text Length")
+        let newText = text.length
+        setText(newText)
+    }
     const handleOnChange = (event) => {
         console.log("OnChnage was clicked")
         setText(event.target.value) //for updating the text field
@@ -18,11 +28,14 @@ export default function TextForm(props) {
 
     return (
         <div>
+            <br></br>
             <h1>{props.name}</h1>
             <div className="mb-3">
-                <textarea className="form-control" value={text} onChange={handleOnChange} id="mybox" rows="8"></textarea>
+                <textarea className="form-control my-3" value={text} onChange={handleOnChange} id="mybox" rows="8"></textarea>
             </div>
             <button className="btn btn-primary" onClick={handleUpClick}>Convert To UpperCase</button>
+            <button className="btn btn-primary mx-3" onClick={handleLoClick}>Convert To LowerCase</button>
+            <button className="btn btn-primary mx-3" onClick={handleLenClick}>Text Length</button>
         </div>
     );
 }
