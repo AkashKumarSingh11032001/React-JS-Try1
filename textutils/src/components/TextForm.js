a667f996234735154d8facbc1dcbac62abe76377
Akash Kumar Singh <akashkumarsingh11032001@gmail.com>
2022-02-04 13:38:20 +0530

    React

--- textutils/src/components/TextForm.js
+++ textutils/src/components/TextForm.js
@@ -18,6 +18,15 @@ export default function TextForm(props) {
         let newText = text.length
         setText(newText)
     }
+    const handleTitClick = () =>{
+        console.log("Title Case")
+        let newText = text.charAt(0).toUpperCase()+text.slice(1)
+        setText(newText)
+    }
+    const handleRevClick = () =>{
+        console.log("reverse text")
+        setText(text.split(" ").reverse().join(" "))
+    }
     const handleOnChange = (event) => {
         console.log("OnChnage was clicked")
         setText(event.target.value) //for updating the text field
@@ -35,9 +44,11 @@ export default function TextForm(props) {
             <div className="mb-3">
                 <textarea className="form-control my-3" value={text} onChange={handleOnChange} id="mybox" rows="8"></textarea>
             </div>
-            <button className="btn btn-primary" onClick={handleUpClick}>Convert To UpperCase</button>
-            <button className="btn btn-primary mx-3" onClick={handleLoClick}>Convert To LowerCase</button>
-            <button className="btn btn-primary mx-3" onClick={handleLenClick}>Text Length</button>
+            <button className="btn btn-primary mx-1" onClick={handleUpClick}>Convert To UpperCase</button>
+            <button className="btn btn-primary mx-1" onClick={handleLoClick}>Convert To LowerCase</button>
+            <button className="btn btn-primary mx-1" onClick={handleTitClick}>Title Case</button>
+            <button className="btn btn-primary mx-1" onClick={handleRevClick}>Reverse Text</button>
+            <button className="btn btn-primary mx-1" onClick={handleLenClick}>Text Length</button>
         </div>
         <div className="container">
             <h2>Your Text Summary</h2>
