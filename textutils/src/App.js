9326525cdd739c98142099647b9ccb1c786df886
Akash Kumar Singh <akashkumarsingh11032001@gmail.com>
2022-02-09 12:47:30 +0530

    React

--- textutils/src/App.js
+++ textutils/src/App.js
@@ -1,32 +1,37 @@
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
 import './App.css';
-// import About from './components/About';
+import About from './components/About';
 import React, { useState } from 'react'
 import Alert from './components/Alert';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
 
 function App() {
-  const [mode,setMode] = useState("light");
-  const[alert,setAlert] = useState(null)
+  const [mode, setMode] = useState("light");
+  const [alert, setAlert] = useState(null)
 
-  const showAlert = (message,type) => {
+  const showAlert = (message, type) => {
     setAlert({
       msg: message,
       type: type
     })
     setTimeout(() => {
       setAlert(null);
-    },1500);
-    
+    }, 1500);
+
   }
-  const toggleMode = () =>{
-    if(mode === "light"){
+  const toggleMode = () => {
+    if (mode === "light") {
       setMode("dark")
       document.body.style.backgroundColor = "#042743";
-      showAlert("Dark Mode Enabled","success")
+      showAlert("Dark Mode Enabled", "success")
       document.title = "TextUtiles - Dark Mode"
     }
-    else{
+    else {
       setMode("light")
       document.body.style.backgroundColor = "white";
       showAlert("Light Mode Enabled")
@@ -35,19 +40,27 @@ function App() {
   }
   return (
     <>
+    <Router>
       {/* NavBar */}
-      <Navbar title="TextUtiles" aboutText="About Us" mode={mode} toggleMode={toggleMode}/>
+      <Navbar title="TextUtiles" aboutText="About Us" mode={mode} toggleMode={toggleMode} />
 
       {/* Alert */}
-      <Alert alert={alert}/>
+      <Alert alert={alert} />
       {/* Text-area */}
-      <div className="container">
-        <TextForm name = "Enter your text here" mode={mode}/>
+      <div className="container my-3">
+        <Routes>
+          <Route exact path="/about" element={<About />} />
+          <Route exact path="/"
+            element = {
+            <TextForm name="Enter your text here" mode={mode} />}
+          />
+        </Routes >
+
       </div>
 
       {/* About -Dark mode */}
-      {/* <About/> */}
-
+      {/* <About /> */}
+      </Router>
     </>
   );
 }
