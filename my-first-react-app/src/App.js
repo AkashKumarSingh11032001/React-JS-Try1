3f4a965b82a1cd144d73db8b5cf5fa7ec6c9b5ed
Akash Kumar Singh <akashkumarsingh11032001@gmail.com>
2022-04-04 21:23:55 +0530

    Styling

--- my-first-react-app/src/App.js
+++ my-first-react-app/src/App.js
@@ -1,24 +1,29 @@
 import React from 'react';
-import {useEffect} from 'react';
+import { useEffect } from 'react';
+import './App.css'
+import SearchIcon from './search.svg'
 // 99f9c1f9
 
 const API_URL = "http://www.omdbapi.com/?i=tt3896198&apikey=99f9c1f9"
 
 const App = () => {
 
+    // getting API data
     const searchMovies = async (title) => {
         const response = await fetch(`${API_URL}&s=${title}`);
         const data = await response.json();
 
         console.log(data.Search);
-    } 
+    }
 
     useEffect(() => {
         searchMovies('IronMan');
-    },[]);
+    }, []);
 
     return (
-        <h1>Hello</h1>
+        <div className="app">
+            <h1>MoviesLand</h1>
+        </div>
     );
 }
 
