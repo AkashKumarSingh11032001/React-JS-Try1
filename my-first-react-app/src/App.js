cba2ba007da0fd89bc470542ac472b21a01e3c2e
Akash Kumar Singh <akashkumarsingh11032001@gmail.com>
2022-04-04 20:18:05 +0530

    init change

--- my-first-react-app/src/App.js
+++ my-first-react-app/src/App.js
@@ -1,41 +1,9 @@
-import './App.css';
-import {useState, useEffect} from 'react';
-// props ---step 1
-// const Person = (props) => {
-//   return(
-//     <>
-//       <h1>Name: {props.name}</h1>
-//       <h2>Age : {props.age}</h2>
-//       <h3>DOB : 11/3/2001 </h3>
-//     </>
-//   );
-// }
+import React from 'react';
 
 const App = () => {
-  const name = "Akash"
-
-  const [counter,setCounter] = useState(0);
-  
-  useEffect(()=>{
-    setCounter(100);
-  },[]);
-
-  return (
-    <div className="App">
-      <h1>Hello React! {name}</h1>
-      {/* props --- step 1 */}
-      {/* <Person name={'John'} age={21}/>
-      <Person name={'Hello'} age={22}/>
-      <Person name={'World'} age={23}/>
-      <Person name={'React'} age={24}/> */}
-
-      {/* event trigger and onClick event (states) */}
-      <button onClick={()=>setCounter((prevCount)=> prevCount-1)}>-</button>
-      <h1>{counter}</h1>
-      <button onClick={()=>setCounter((prevCount)=> prevCount+1)}>+</button>
-    
-    </div>
-  );
+    return (
+        <h1>Hello</h1>
+    );
 }
 
 export default App;
