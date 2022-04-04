4e3fba7ee8901f5760e4eb3a3de2dbaef558a034
Akash Kumar Singh <akashkumarsingh11032001@gmail.com>
2022-04-04 20:32:41 +0530

    serach

--- my-first-react-app/src/App.js
+++ my-first-react-app/src/App.js
@@ -1,6 +1,22 @@
 import React from 'react';
+import {useEffect} from 'react';
+// 99f9c1f9
+
+const API_URL = "http://www.omdbapi.com/?i=tt3896198&apikey=99f9c1f9"
 
 const App = () => {
+
+    const searchMovies = async (title) => {
+        const response = await fetch(`${API_URL}&s=${title}`);
+        const data = await response.json();
+
+        console.log(data.Search);
+    } 
+
+    useEffect(() => {
+        searchMovies('IronMan');
+    },[]);
+
     return (
         <h1>Hello</h1>
     );
